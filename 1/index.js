42e089738cd05aedc9d19c3509b46b894272928b
Jonah Snider <me@jonahsnider.ninja>
2018-11-30 21:49:56 -0800

    feat: Part 2 support

--- 1/index.js
+++ 1/index.js
@@ -1,13 +1,22 @@
 const lineByLine = require('linebyline');
 let result = 0;
+const seen = [0];
+let twice;
 
 lineByLine('input.txt')
   // Each line read triggers this event
-  .on('line', change => {
-    result += parseInt(change.replace('+', ''), 10);
+  .on('line', frequency => {
+    const change = parseInt(frequency, 10);
+    result += change;
+
+    if (seen.includes(change) && !twice) {
+      twice = change;
+    }
+
+    seen.push(change);
   })
   // When all lines have been read this event is triggered
   .on('end', () => {
-    console.log(`Finished! Final value is ${result}`);
+    console.log(`Finished! Final value is ${result}. First value reached twice is ${twice}`);
   })
   .on('error', err => console.error('Error while reading in lines from input.txt', err));
