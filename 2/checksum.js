dd46c3951cfd9f9f1c248f974e296877e33ac05d
Jonah Snider <me@jonahsnider.ninja>
2018-12-02 13:19:52 -0800

    feat: Optimize `checksum` function

--- 2/checksum.js
+++ 2/checksum.js
@@ -7,19 +7,46 @@ module.exports = (file = 'input.txt') => {
   let thrice = 0;
 
   lines.forEach(string => {
-    const count = new Map();
+    const count = {
+      a: 0,
+      b: 0,
+      c: 0,
+      d: 0,
+      e: 0,
+      f: 0,
+      g: 0,
+      h: 0,
+      i: 0,
+      j: 0,
+      k: 0,
+      l: 0,
+      m: 0,
+      n: 0,
+      o: 0,
+      p: 0,
+      q: 0,
+      r: 0,
+      s: 0,
+      t: 0,
+      u: 0,
+      v: 0,
+      w: 0,
+      x: 0,
+      y: 0,
+      z: 0
+    };
 
     // Count how many times each letter appears
     for (let i = 0; i < string.length; i++) {
       const char = string[i];
 
-      count.set(char, (count.get(char) || 0) + 1);
+      count[char] = (count[char] || 0) + 1;
     }
 
     let foundTwice = false;
     let foundThrice = false;
 
-    for (const occurrences of count.values()) {
+    for (const occurrences in count) {
       if (occurrences === 2 && !foundTwice) {
         twice++;
         foundTwice = true;
--- 2/index.js
+++ 2/index.js
@@ -4,9 +4,13 @@ const { join } = require('path');
 
 const file = join(__dirname, 'input.txt');
 
+const start = Date.now();
+
 const { checksum, twice, thrice } = checksumFn(file);
 
-console.log('Checksum:', checksum);
+const end = Date.now() - start;
+
+console.log(`Checksum (took ${end}ms):`, checksum);
 console.log('Twice occurrences:', twice);
 console.log('Thrice occurrences:', thrice);
 
